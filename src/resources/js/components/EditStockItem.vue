bca66fa23ba5c2acf034d5fe5f91f9f55d5a84a2
James <james.convery@plotbox.io>
2023-03-05 19:29:09 +0000

    edit stock item form updated

--- src/resources/js/components/EditStockItem.vue
+++ src/resources/js/components/EditStockItem.vue
@@ -5,83 +5,166 @@
             Add Stock Item
         </span>
     </div>
-  <div class="relative overflow-x-auto m-2 p-2">
-    <form v-if="this.stockItem !== null">
-        <div class="mb-6">
-            <label for="name"
-                class="block mb-2 text-sm font-medium text-gray-900"
-            >
-                Name
-            </label>
-            <input
-                v-model="stockItem.name"
-                type="text"
-                id="name"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-3/4 p-2.5"
-            required>
-        </div>
-        <div class="mb-6">
-            <label for="name"
-                class="block mb-2 text-sm font-medium text-gray-900"
-            >
-                Supplier
-            </label>
-            <input
-                v-model="stockItem.supplier_id"
-                type="text"
-                id="name"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5"
-            required>
-        </div>
-        <div class="mb-6">
-            <label for="name"
-                class="block mb-2 text-sm font-medium text-gray-900"
-            >
-                Shoe Size
-            </label>
-            <input
-                v-model="stockItem.shoe_size"
-                type="text"
-                id="name"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/5 p-2.5"
-            required>
-        </div>
-        <div class="mb-6">
-            <label for="name"
-                class="block mb-2 text-sm font-medium text-gray-900"
-            >
-                Material
-            </label>
-            <input
-                v-model="stockItem.material"
-                type="text"
-                id="name"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5"
-            required>
-        </div>
-        <div class="mb-6">
-            <label for="name"
-                class="block mb-2 text-sm font-medium text-gray-900"
-            >
-                Category
-            </label>
-            <input
-                v-model="stockItem.category"
-                type="text"
-                id="name"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5"
-            required>
-        </div>
-        <div class="flex justify-end pr-4">
-            <button
-                @click="editStockItem"
-                type="button"
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
-            >
-                Save
-            </button>
-        </div>
-    </form>
+    <div class="relative overflow-x-auto m-2 p-2">
+        <form v-if="this.dataFetched">
+            <div class="grid grid-cols-2">
+                <div>
+                    <div class="flex mb-12">
+                        <label
+                            for="name"
+                            class="block ml-10 w-32 text-sm font-medium text-gray-900"
+                        >
+                            Name
+                        </label>
+                        <input
+                            v-model="stockItem.name"
+                            type="text"
+                            id="name"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5"
+                            required
+                        >
+                    </div>
+                    <div class="flex mb-12">
+                        <label for="name"
+                            class="block ml-10 w-32 text-sm font-medium text-gray-900"
+                        >
+                            Supplier
+                        </label>
+                        <!-- Dropdown menu -->
+                        <select v-model="stockItem.supplier_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5">
+                            <option disabled value="">Select a supplier</option>
+                            <option :key="supplier.id" v-for="supplier in suppliers" :value="supplier.id">
+                                {{ supplier.name }}
+                            </option>
+                        </select>
+                    </div>
+                    <div class="flex mb-12">
+                        <label for="name"
+                            class="block ml-10 w-32 text-sm font-medium text-gray-900"
+                        >
+                            Gender
+                        </label>
+                        <!-- Dropdown menu -->
+                        <select v-model="stockItem.gender" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5">
+                            <option disabled value="">Select a gender</option>
+                            <option :key="gender.id" v-for="gender in genders" :value="gender.name">
+                                {{ gender.name }}
+                            </option>
+                        </select>
+                    </div>
+                    <div class="flex mb-12">
+                        <label for="name"
+                            class="block ml-10 w-32 text-sm font-medium text-gray-900"
+                        >
+                            Brand
+                        </label>
+                        <!-- Dropdown menu -->
+                        <select v-model="stockItem.brand" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5">
+                            <option disabled value="">Select a brand</option>
+                            <option :key="brand.id" v-for="brand in brands" :value="brand.value">
+                                {{ brand.value }}
+                            </option>
+                        </select>
+                    </div>
+                    <div class="flex mb-12">
+                        <label for="name"
+                            class="block ml-10 w-32 text-sm font-medium text-gray-900"
+                        >
+                            Colour
+                        </label>
+                        <!-- Dropdown menu -->
+                        <select v-model="stockItem.colour" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5">
+                            <option disabled value="">Select a colour</option>
+                            <option :key="colour.id" v-for="colour in colours" :value="colour.name">
+                                {{ colour.name }}
+                            </option>
+                        </select>
+                    </div>
+                    <div class="flex mb-12">
+                        <label for="name"
+                            class="block ml-10 w-32 text-sm font-medium text-gray-900"
+                        >
+                            Shoe Size
+                        </label>
+                        <input
+                            v-model="stockItem.shoe_size"
+                            type="number"
+                            min="1"
+                            step="any"
+                            id="name"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/4 p-2.5"
+                        required>
+                    </div>
+                </div>
+                <div>
+                    <div class="flex mb-12">
+                        <label for="name"
+                            class="block w-32 text-sm font-medium text-gray-900"
+                        >
+                            Material
+                        </label>
+                        <select v-model="stockItem.material" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5">
+                            <option disabled value="">Select a material</option>
+                            <option :key="material.id" v-for="material in materials" :value="material.value">
+                                {{ material.value }}
+                            </option>
+                        </select>
+                    </div>
+                    <div class="flex mb-12">
+                        <label for="name"
+                            class="block w-32 text-sm font-medium text-gray-900"
+                        >
+                            Category
+                        </label>
+                        <select v-model="stockItem.category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5">
+                            <option disabled value="">Select a category</option>
+                            <option :key="category.id" v-for="category in categories" :value="category.value">
+                                {{ category.value }}
+                            </option>
+                        </select>
+                    </div>
+                    <div class="flex mb-12">
+                        <label for="name"
+                            class="block w-32 text-sm font-medium text-gray-900"
+                        >
+                            Price (£)
+                        </label>
+                        <input
+                            v-model="stockItem.price"
+                            type="number"
+                            id="price"
+                            min="1"
+                            step="any"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/4 p-2.5"
+                        required>
+                    </div>
+                    <div class="flex mb-12">
+                        <label for="name"
+                            class="block w-32 text-sm font-medium text-gray-900"
+                        >
+                            Stock Level
+                        </label>
+                        <input
+                            v-model="stockItem.stock_level"
+                            type="number"
+                            id="price"
+                            min="1"
+                            step="any"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/4 p-2.5"
+                        required>
+                    </div>
+                </div>
+            </div>
+            <div class="flex justify-end pr-4">
+                <button
+                    @click="editStockItem"
+                    type="button"
+                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+                >
+                    Save
+                </button>
+            </div>
+        </form>
     <div v-else class="grid min-h-screen place-content-center">
         <div class="flex items-center gap-2 text-gray-500">
         <span class="h-6 w-6 block rounded-full border-4 border-t-blue-300 animate-spin"></span>
@@ -102,11 +185,90 @@ export default {
     },
     data() {
         return {
-            stockItem: null
+            stockItem: null,
+            brands: [],
+            categories: [],
+            materials: [],
+            suppliers: [],
+            genders: [
+                {
+                    id: 1,
+                    name: 'men'
+                },
+                {
+                    id: 2,
+                    name: 'women'
+                },
+                {
+                    id: 3,
+                    name: 'unisex'
+                },
+                {
+                    id: 4,
+                    name: 'kids'
+                }
+            ],
+            colours: [
+                {
+                    id: 1,
+                    name: 'Black'
+                },
+                {
+                    id: 2,
+                    name: 'Blue'
+                },
+                {
+                    id: 3,
+                    name: 'Brown'
+                },
+                {
+                    id: 4,
+                    name: 'Green'
+                },
+                {
+                    id: 5,
+                    name: 'Grey'
+                },
+                {
+                    id: 6,
+                    name: 'Multi'
+                },
+                {
+                    id: 7,
+                    name: 'Orange'
+                },
+                {
+                    id: 8,
+                    name: 'Pink'
+                },
+                {
+                    id: 9,
+                    name: 'Purple'
+                },
+                {
+                    id: 10,
+                    name: 'Red'
+                },
+                {
+                    id: 11,
+                    name: 'White'
+                },
+                {
+                    id: 12,
+                    name: 'Yellow'
+                }
+            ]
         }
     },
     mounted() {
         this.getStockItem(this.$route.params.id);
+        this.getSuppliers();
+        this.getDropdowns();
+    },
+    computed: {
+        dataFetched() {
+            return this.stockItem && this.suppliers && this.brands && this.categories && this.materials !== null;
+        }
     },
     methods: {
         getStockItem(id) {
@@ -124,6 +286,26 @@ export default {
                     errors.value = error.response.data.errors;
                 }
             }
+        },
+        getSuppliers() {
+            axios.get("http://127.0.0.1:8080/api/suppliers")
+                .then((response) => {
+                    this.suppliers = response.data.data;
+                });
+        },
+        getDropdowns() {
+            axios.get("http://127.0.0.1:8080/api/dropdown_group/" + 'brand')
+                .then((response) => {
+                    this.brands = response.data;
+                });
+            axios.get("http://127.0.0.1:8080/api/dropdown_group/" + 'category')
+                .then((response) => {
+                    this.categories = response.data;
+                });
+            axios.get("http://127.0.0.1:8080/api/dropdown_group/" + 'material')
+                .then((response) => {
+                    this.materials = response.data;
+                });
         }
     }
 }
