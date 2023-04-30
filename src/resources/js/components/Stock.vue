88ceff523109b84b38b314a82ae06b0798019336
James <james.convery@plotbox.io>
2023-04-30 03:35:34 +0100

    warning icon added first draft

--- src/resources/js/components/Stock.vue
+++ src/resources/js/components/Stock.vue
@@ -120,7 +120,16 @@
                         {{ stockItem.category }}
                     </td>
                     <td class="pl-4 py-4">
-                        {{ stockItem.stock_level }}
+                        <div class="flex">
+                            <div>
+                                {{ stockItem.stock_level }}
+                            </div>
+                            <div>
+                                <svg aria-hidden="true" class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white ml-1" fill="red" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path clip-rule="evenodd" d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495zM10 5a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 5zm0 9a1 1 0 100-2 1 1 0 000 2z" fill-rule="evenodd"></path>
+                                </svg>
+                            </div>
+                        </div>
                     </td>
                     <td class="py-4 pl-4 space-x-2">
                     <button
